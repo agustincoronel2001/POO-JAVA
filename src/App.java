f12245bae70e96d9a5073b9170624bfd499e7b87
agustin <agustincoronel2001@gmail.com>
2025-08-22 14:40:58 -0300

    Aprendiendo POO en JAVA

---
+++ src/App.java
@@ -0,0 +1,44 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Persona persona1 = new Persona();
+        persona1.nombre="Agustin";
+        persona1.apellido="Coronel";
+        persona1.edad= 53;
+
+        Carrera carrera1 = new Carrera();
+        carrera1.nombre="Ingenieria en computacion";
+        carrera1.duracion = 6;
+        carrera1.estaEnCurso=false;
+
+        persona1.carrera= carrera1;
+
+        Persona persona2 = new Persona();
+        persona2.nombre = "Mariana";
+        persona2.apellido="Perez";
+        persona2.edad= 24;
+
+        Carrera carrera2 = new Carrera();
+        carrera2.nombre="Ingenieria en Sistemas";
+        carrera2.duracion = 6;
+        carrera2.estaEnCurso=true;
+
+        persona2.carrera=carrera2;
+
+
+
+        String saludado = "Sergie ";
+
+        System.out.println(persona1.apellido + " , " + persona1.nombre + " tiene " + persona1.edad + " años .");
+        System.out.println(persona2.apellido + " , " + persona2.nombre + " tiene " + persona2.edad + " años .");
+
+        System.out.println(persona1.darNombreCompleto() + " tiene " + persona1.edad + " años y esta recibido de " + persona1.carrera.nombre ); 
+        System.out.println(persona2.darNombreCompleto() + " tiene " + persona2.edad + " años y esta cursando " + persona2.carrera.nombre );
+        System.out.println(persona2.enviarSaludo(saludado));
+        System.out.println(persona2.enviarSaludo("a otro"));
+
+        System.out.println(persona1.enviarSaludo(saludado));
+        System.out.println(persona2.enviarSaludo("Luke Skywalker"));
+
+
+    }
+}
---
+++ src/Persona.java
@@ -0,0 +1,18 @@
+public class Persona {
+       //Atributos (estado/caracteristicas de un objeto)
+        String nombre;
+        String apellido;
+        int edad;
+        Carrera carrera;
+        //Métodos (comportamiento de un objeto)
+
+        public String darNombreCompleto(){
+                return apellido + " , " + nombre;
+        }
+
+        public String enviarSaludo(String saludado){
+                if(edad > 40) return "Buenos dias, querido " + saludado;
+                return "Hola, ¿Como estas? " + saludado + " ? ";
+        }
+
+}
